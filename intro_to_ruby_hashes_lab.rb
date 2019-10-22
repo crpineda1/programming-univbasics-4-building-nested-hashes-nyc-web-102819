a651b955d99e349141480a466992c3ae28235609
Carlos Pineda <carlos.r.pineda@gmail.com>
2019-10-22 18:08:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,8 @@ def monopoly_with_fourth_tier
   # complexity.
   monopoly = {
     railroads: {
-      pieces: 4,rent_in_dollars: {
+      pieces: 4,
+      rent_in_dollars: {
         one_piece_owned:25,
         two_pieces_owned:50,
         three_pieces_owned:100,
